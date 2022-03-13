42c18889826f1b06e558afa79d6e6d5f3101dc32
nitishnaidi <nitishnaidi@gmail.com>
2022-03-13 16:51:11 -0500

    dynamic routing implemented with clicks and styled nav items

--- src/app/side-nav/side-nav.component.ts
+++ src/app/side-nav/side-nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-side-nav",
@@ -7,12 +8,16 @@ import { Component, OnInit } from "@angular/core";
 })
 
 export class SideNavComponent implements OnInit {
-  
-  constructor() { 
+  selectedItem = 'info';
+  constructor(private router: Router) { 
 
   }
 
-  ngOnInit() {
+  ngOnInit() { }
 
+  onClick(item: string) {
+    console.log('clicked item is: ', item);
+    this.selectedItem = item;
+    this.router.navigateByUrl(this.selectedItem);
   }
 }
