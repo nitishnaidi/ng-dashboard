f5624b9426ef152e3684e1e1e446d5f956097a60
nitishnaidi <nitishnaidi@gmail.com>
2022-04-04 21:22:20 -0500

    implemented sum operation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
@@ -7,6 +8,7 @@ import { ApplyComponent } from './features/apply/apply.component';
 import { GiveComponent } from './features/give/give.component';
 import { HomeComponent } from './features/home/home.component';
 import { RequestInfoComponent } from './features/request-info/request-info.component';
+import { SumComponent } from './features/sum/sum.component';
 import { VisitComponent } from './features/visit/visit.component';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
@@ -22,9 +24,10 @@ import { SideNavComponent } from './side-nav/side-nav.component';
     GiveComponent,
     RequestInfoComponent,
     ApplyComponent,
-    HomeComponent
+    HomeComponent,
+    SumComponent
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
