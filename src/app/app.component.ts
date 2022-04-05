f1e71edbcdc21e43d2e7d5873511f930668dae57
nitishnaidi <nitishnaidi@gmail.com>
2022-04-04 21:22:20 -0500

    implemented sum operation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
+  x=0; y=0; z=0;
   isHomeClicked = 'default';
 
   handleHomeClickEvent(value: string) {
+    console.log('handler', value);
+    
     this.isHomeClicked = value;
+  
   }
+  
 }
