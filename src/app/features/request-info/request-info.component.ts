362b3d90012f312a71022993188f7ebbbdf94a76
snehankithachennuri <chennurisnehankitha@gmail.com>
2022-04-05 18:21:42 -0500

    implemented auto complete

--- src/app/features/request-info/request-info.component.ts
+++ src/app/features/request-info/request-info.component.ts
@@ -9,6 +9,7 @@ export class RequestInfoComponent {
   selectedCountryAdvanced: any;
   lastSelectedCountry: any;
   countries: any[] = [];
+  countrySuggestions: any[] = [];
   ngOnInit() {
     this.countries = [
       { label: 'Germany', value: 'ge' },
@@ -27,4 +28,21 @@ export class RequestInfoComponent {
   processOnClick() {
     console.log('processOnClick');
   }
+  filterCountry(event: any){
+    console.log("filter country",event);
+    this.countrySuggestions=[];
+    // this.countrySuggestions= this.countries;
+    // for (let index = 0; index < this.countries.length; index++) {
+    //   const country = this.countries[index];
+    //   if(country.label.toLowerCase().includes(event.query.toLowerCase())){
+    //     this.countrySuggestions.push(country);
+    //   }
+    // }
+
+    this.countrySuggestions = this.countries.filter(country => country.label.toLowerCase().includes(event.query.toLowerCase()));
+    
+
+  }
+
+
 }
